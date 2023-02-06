d7e3bdb356528fb87676bd6b02272a51a91f9933
b-rbmp <4665417+b-rbmp@users.noreply.github.com>
2023-02-05 22:06:55 -0300

    Updates the backend provider from railway to render

--- src/fetching/api.ts
+++ src/fetching/api.ts
@@ -4,25 +4,25 @@ import { TGlobalData, TWeatherStation, TNewWeatherStation, TEstacaoData } from "
 
 export const getWeatherStations = async() => {
     const { data }: {data: TWeatherStation[]} = await axios.get(
-        "https://weatherlogger-production.up.railway.app/weatherlogger/api/v1/weatherstation/"
+        "https://weatherlogger-backend.onrender.com/weatherlogger/api/v1/weatherstation/"
       );
     return data;
 }
 
 export const getGlobalData = async() => {
     const { data }: {data: TGlobalData} = await axios.get(
-        "https://weatherlogger-production.up.railway.app/weatherlogger/api/v1/globaldata/"
+        "https://weatherlogger-backend.onrender.com/weatherlogger/api/v1/globaldata/"
       );
     return data;
 }
 
 export const getEstacaoData = async(weatherStationId: number) => {
     const { data }: {data: TEstacaoData} = await axios.get(
-        "https://weatherlogger-production.up.railway.app/weatherlogger/api/v1/estacaodata/"+weatherStationId.toString()
+        "https://weatherlogger-backend.onrender.com/weatherlogger/api/v1/estacaodata/"+weatherStationId.toString()
       );
     return data;
 }
 
 export const postEstacao = async(newWeatherStation: TNewWeatherStation) => {
-    return axios.post('https://weatherlogger-production.up.railway.app/weatherlogger/api/v1/weatherstation/', newWeatherStation)
+    return axios.post('https://weatherlogger-backend.onrender.com/weatherlogger/api/v1/weatherstation/', newWeatherStation)
 }
